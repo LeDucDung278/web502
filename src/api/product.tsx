c3101c37dd6901dc19b7905ca21343a9f7e57a40
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-04-11 00:49:06 +0700

    check access admin

--- src/api/product.tsx
+++ src/api/product.tsx
@@ -1,23 +1,45 @@
 import { ProductType } from "../type/Product"
 import instance from "./instance"
+import { isAuthenticate } from "../utils/localStorage";
 
-export const list = () =>{
+const user = isAuthenticate()
+
+export const list = () => {
     const url = `/products`
     return instance.get(url)
 }
-export const read = (id:number) =>{
+export const read = (id: string | undefined) => {
     const url = `/products/${id}`
     return instance.get(url)
 }
-export const remove = (_id:number) =>{
-    const url = `/products/${_id}`
-    return instance.delete(url)
+export const remove = (_id?: number | undefined) => {
+    const url = `/products/${_id}/${user?.user._id}`
+    return instance.delete(url,
+        {
+            headers: {
+                "Authorization": `Bearer ${user?.token}`
+            }
+        })
 }
-export const create = (product: ProductType) =>{
-    const url = `/products`
-    return instance.post(url,product)
+export const create = (product: ProductType) => {
+    const url = `/products/${user?.user._id}`
+    return instance.post(url, product,
+        {
+            headers: {
+                "Authorization": `Bearer ${user?.token}`
+            }
+        })
 }
 export const update = (product: ProductType) => {
-    const url = `/products/${product._id}`
-    return instance.put(url,product)
+    const url = `/products/${product._id}/${user?.user._id}`
+    return instance.put(url, product,
+        {
+            headers: {
+                "Authorization": `Bearer ${user?.token}`
+            }
+        })
+}
+export const search = (search_value: string | undefined) => {
+    const url = `/search/?q=${search_value}`
+    return instance.post(url)
 }
