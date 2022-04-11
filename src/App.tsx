29ed42781d51085c2f40a463a3f5f455eead4250
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-04-12 00:19:28 +0700

    add upload img

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import { ToastContainer, toast} from 'react-toastify';
 import "react-toastify/dist/ReactToastify.min.css"
 import { create, list, remove, update } from './api/product'
@@ -15,10 +15,18 @@ import SignUp from './pages/SignUp'
 import { ProductType } from './type/Product'
 import ProductAdd from './components/ProductAdd'
 import ProductEdit from './components/ProductEdit'
+import Search from './pages/Search';
+import HomePage from './pages/HomePage';
+import ProductLayout from './pages/layout/ProductLayout';
+import PrivateRouter from './components/PrivateRouter';
+import Error from './pages/Error';
+import CategoryList from './components/CategoryList';
+import axios from 'axios';
 
 function App() {
-
+    
     const [products, setProducts] = useState<ProductType[]>([])
+    const navigate = useNavigate()
     useEffect(() => {
         const getProducts = async () => {
             const { data } = await list();
@@ -35,11 +43,26 @@ function App() {
     }
 
     const onHandleAdd = async (product: any) => {
-        console.log('app.s', product)
-
-        const { data } = await create(product)
-
-        setProducts([...products, data])
+        try {
+            const apiUrl = "https://api.cloudinary.com/v1_1/dfwailscz/image/upload"
+            const image = product.img[0]
+            const formdata = new FormData()
+            formdata.append("file",image)
+            formdata.append("upload_preset","ass_react")
+            const {data} = await axios.post(apiUrl, formdata,{
+                headers: {
+                    "Content-type": "application/form-data",
+                }
+            })
+            const addproduct = await create({...product, img:data.url})
+            toast.success("Bạn đã thêm sản phẩm thành công")
+                //...product mảng ban đầu sét vơi product mới thêm
+            setProducts([...product, addproduct.data])
+            setTimeout(()=> navigate("/admin/product"), 3000)
+        } catch (error) {
+            toast.error("Thêm sản phẩm không thành công")
+            console.log(error)
+        }
     }
 
     const onHandleUpdate = async (product: ProductType) => {
@@ -56,19 +79,28 @@ function App() {
             <Routes>
                 <Route path='/' element={<WebsiteLayout />}>
                     <Route index element={<ProductList products={products} />} />
+                    <Route path='product/search/:search_value' element={<Search/>}/>
+                    {/* <Route path='/dung' element={<HomePage/>} products={products}/> */}
                 </Route>
+                <Route path='/product' element={<ProductLayout />}>
+                    <Route index element={<HomePage products={products} />} />
+                    <Route path='category/:id' element={<CategoryList />}/>
+                </Route>
+
+                
 
                 <Route path='product/:id' element={<ProductDetail />} />
 
-                <Route path='/admin' element={<AdminLayout />}>
+                <Route path='/admin' element={<PrivateRouter><AdminLayout /></PrivateRouter>}>
                     <Route index element={<Dashboard />} />
                     <Route path='product' element={<ProductManager products={products} onRemove={onHandleRemove} />} />
                     <Route path='product/add' element={<ProductAdd onAdd={onHandleAdd} />} />
                     <Route path='product/:id/edit' element={<ProductEdit onUpdate={onHandleUpdate} />} />
                 </Route>
                 
-                <Route path='/signup'element={<SignUp/>}/>
-                <Route path='/signin'element={<SignIn/>}/>
+                <Route path='/signup'element={<SignUp name={''} email={''} password={''}/>}/>
+                <Route path='/signin'element={<SignIn email={''} password={''}/>}/>
+                <Route path='/error'element={<Error/>}/>
             </Routes>
             <ToastContainer />
         </div>
