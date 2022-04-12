b58637b841f480ec85cb218220586036b851d3c1
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-04-12 09:39:07 +0700

    productList

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,35 +2,41 @@ import React from 'react'
 import { ProductType } from '../type/Product'
 
 type ProductListProps = {
-  products : ProductType[]
+    products: ProductType[]
 }
 
-const ProductList = ({products}: ProductListProps) => {
-  return (
-    <div className="container mx-auto px-6">
-      {/* <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
+const ProductList = ({ products }: ProductListProps) => {
+    return (
+        <div className="max-w-2xl mx-auto py-2 px-4 sm:py-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            {/* <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
       <span className="mt-3 text-sm text-gray-500">200+ Products</span> */}
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-        {products?.map(item =>{
-          return (
-          <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-          <div className="flex items-end justify-end h-56 w-full bg-cover" style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80")' }}>
-            <button className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-              <svg className="h-5 w-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-            </button>
-          </div>
-          <div className="px-5 py-3">
-            <a href={`/product/${item._id}`}>
-              <h3  className="text-gray-700 uppercase">{item.name}</h3>
-              <span className="text-gray-500 mt-2">{item.price}</span>
-            </a>
-          </div>
+            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Sản Phẩm Mới</h2>
+            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                {products?.map((item, index) => {
+                    return (
+                        <div key={index} className="group relative boder">
+                            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                                <img src={item.img} alt="Front of men's Basic Tee in black." className="w-full h-full object-center object-cover lg:w-full lg:h-full" />
+                            </div>
+
+                            <div className="mt-4 flex justify-between">
+                                <div>
+                                    <h3 className="text-sm text-gray-700">
+                                        <a href={`/product/${item._id}`}>
+                                            <span aria-hidden="true" className="absolute inset-0"></span>
+                                            {item.name}
+                                        </a>
+                                    </h3>
+                                    <p className="mt-1 text-sm text-gray-500">Black</p>
+                                </div>
+                                <p className="text-sm font-medium text-gray-900">${item.price}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
-          )
-        })}
-      </div>
-    </div>
-  )
+    )
 }
 
 export default ProductList
